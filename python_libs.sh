ba2658cfc3c65b277288da2004182b577e20cfcf
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-11-16 14:31:27 -0500

    Create python_libs.sh

---
+++ python_libs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# install python stuff
+pip install lxml
+pip install cheetah
+pip install sphinx
+pip install numpy
+pip install matplotlib
+pip install pillow
+pip install pandas
+pip install scipy
+pip install scapy
+pip install scikit-learn
+pip install statsmodels
+pip install "ipython[all]"
+pip install git+git://github.com/geopandas/geopandas.git
