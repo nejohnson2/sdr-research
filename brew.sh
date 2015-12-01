393b75a22dc6a5794ad89fd82875445cbede7c26
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-12-01 12:31:34 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
-
 # Ask for the administrator password upfront.
 sudo -v
 
@@ -14,7 +12,6 @@ if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
 
 # Make sure we’re using the latest Homebrew.
 brew update
